fda90f0ea2b321eb98e00c69331b3ad8453f26cf
Angel Ivana <i.angeell@gmail.com>
2023-01-24 18:14:58 +0200

    initial commit

---
+++ src/main/java/com/iasys/webapppoc/WebapppocApplication.java
@@ -0,0 +1,13 @@
+package com.iasys.webapppoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebapppocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebapppocApplication.class, args);
+	}
+
+}
